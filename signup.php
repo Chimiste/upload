973752292d603625d2af122edde9dd000b77cccf
chamssoudine bacar <chamssoudinebacar@yahoo.fr>
2015-05-21 12:28:43 +0600

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,7 @@
+<input type="file" name="user_photo" id="user_photo" 
+                   onchange="ajaxFileUpload('<?php echo  base_url() ?>user-photo', '.create-user-form', 'user_photo')" />
+                   <input type="hidden" name="user_photo" id="user_photo"  value=""/>
+                   
+                   
+                   
+                   <img src="" id="user_pic_dir">
